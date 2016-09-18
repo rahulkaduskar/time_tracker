b3f9de3ff0a74440497b4d544261ce29fe1a8359
Rahul Kaduskar <rahulk@Rahuls-MacBook-Pro.local>
2016-09-18 12:29:59 +0530

    Adding seed data for testing...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..10).each do |n|
+  user = User.new(username: "user#{n}")
+  user.save!
+end
+User.all.each do |user|
+  (0..3).each do |n|
+    user_arrival = UserArrival.new()
+    user_arrival.user = user
+    user_arrival.save(:validate => false)
+    time = (Time.now - (n-1).days - rand(200).minutes)
+    user_arrival.update_attribute(:arrived_at, time )
+ end
+end
